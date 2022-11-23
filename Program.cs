e520344433db754fa1b9c2fd154511f73b5bb40f
AnimewkaSes <brol0526@gmail.com>
2022-11-23 17:46:26 +0600

    Задание 2 Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+        
+        
+        Console.Write("Введите первое число ");
+        string firstValue = Console.ReadLine();
+        int first = Convert.ToInt32(firstValue);
+
+        Console.Write("Введите второе число ");
+        string secondValue = Console.ReadLine();
+        int second = Convert.ToInt32(secondValue);
+
+
+        if (first>second)
+        {
+            Console.WriteLine ("Наибольшее число " + firstValue);
+        } else
+        {
+            Console.WriteLine("Наибольшее число "+secondValue);
+        }
+       
+       
+    }
+}
